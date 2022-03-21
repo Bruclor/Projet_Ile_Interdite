eb1fe82e7a608b4156010714ff4360a9f1ec329a
lbrucat <lorenzo.brucato@u-psud.fr>
2022-03-21 17:10:37 +0100

    maj

--- src/Ile.java
+++ src/Ile.java
@@ -1,3 +1,5 @@
 public class Ile {
     private int taille;
 }
+
+
--- src/Zone.java
+++ src/Zone.java
@@ -1,5 +1,12 @@
+enum Artefact {Eau, Terre, Feu, Air, Heliport}
+
+enum Etat {Normale, Inondee, Submergee}
+
 public class Zone {
+
     private int x;
     private int y;
+    private Etat etat;
+    private Artefact artefact;
 
 }
