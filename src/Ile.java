fc8a9e8781f95f2ed371d55a1494bac760ed87fe
lbrucat <lorenzo.brucato@u-psud.fr>
2022-03-21 17:41:32 +0100

    maj

--- src/Ile.java
+++ src/Ile.java
@@ -1,5 +1,23 @@
-public class Ile {
-    private int taille;
+public class Ile extends Zone {
+
+    private final int taille;
+    private Zone[][] grille = new Zone[this.taille][this.taille];
+
+    /**
+     * Constructeurs
+     *  @param x
+     * @param y
+     * @param etat
+     * @param artefact
+     * @param taille
+     **/
+    public Ile(int x, int y, Etat etat, Artefact artefact, int taille) {
+        super(x, y, etat, artefact);
+        this.taille = taille;
+    }
+
+
+
 }
 
 
