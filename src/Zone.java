302d77a55a945cabe97be50e0b36031c588aa155
lbrucat <lorenzo.brucato@u-psud.fr>
2022-03-21 17:15:56 +0100

    maj

--- src/Zone.java
+++ src/Zone.java
@@ -4,9 +4,20 @@ enum Etat {Normale, Inondee, Submergee}
 
 public class Zone {
 
+    /** Attributs **/
     private int x;
     private int y;
     private Etat etat;
     private Artefact artefact;
+
+    /** Constructeurs **/
+    public Zone(int x, int y, Etat etat, Artefact artefact){
+        this.x = x;
+        this.y = y;
+        this.etat = etat;
+        this.artefact = artefact;
+    }
+
+    /** Methodes **/
 
 }
