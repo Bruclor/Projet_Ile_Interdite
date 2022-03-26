d16bc51ffe3cc413118bebde0d1af5e9d6a97c85
alexandre <alexou.loret@gmail.com>
2022-03-26 13:11:43 +0100

    Debut interface graphique

---
+++ src/ZoneWindow.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+// j'ai pris ca sur internet
+public class ZoneWindow extends JFrame {
+
+    public ZoneWindow(Ile ile) {
+
+        super("Ile Interdite");
+
+        WindowListener l = new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        };
+
+        addWindowListener(l);
+
+        ImageIcon img = new ImageIcon("tips.gif");
+        JButton bouton = new JButton("Fin de tour", img);
+
+        JPanel panneau = new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(Color.BLUE);
+                for (int x=0; x<ile.getTaille(); x++){
+                    for (int y=0; y< ile.getTaille(); y++) {
+                        if (ile.getGrille()[x][y].etat() == Etat.Normale)g.setColor(Color.ORANGE.brighter());
+                        else if (ile.getGrille()[x][y].etat() == Etat.Inondee)g.setColor(Color.BLUE.brighter());
+                        g.fillRect(105+x*100, 105+y*100, 90, 90);
+                        g.setColor(Color.BLUE);
+                    }
+                }
+
+            }
+        };
+
+        panneau.add(bouton);
+        setContentPane(panneau);
+        setSize(200, 100);
+        setVisible(true);
+    }
+}
