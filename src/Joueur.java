e5fe2f717d1882d6d2a61d2309630fea40820632
Bruclor <102045677+Bruclor@users.noreply.github.com>
2022-03-26 19:43:38 +0100

    Create Joueur.java

---
+++ src/Joueur.java
@@ -0,0 +1,25 @@
+public class Joueur {
+  
+  /* identifiant et nom du joueur */
+  private int id;
+  private String nom;
+  
+  /* Statut */
+  private boolean enJeu;
+  private int x;   /* pour x et y faire une classe coord + extends ? */
+  private int y;
+  
+  /* Nombre de clés associées à chaque élément */
+  private int Cle_Feu;
+  private int Cle_Eau;
+  private int Cle_Terre;
+  private int Cle_Air;
+  
+  /* Artefact en possession */
+  private int Art_Feu;
+  private int Art_Eau;
+  private int Art_Terre;
+  private int Art_Air;
+  
+}
+  
