aff790e272d1bf4282e4b215fcdac05a0443508b
Alexandre Loret <102045641+DarenXL@users.noreply.github.com>
2022-03-27 11:51:03 +0200

    Update ZoneWindow.java

--- src/ZoneWindow.java
+++ src/ZoneWindow.java
@@ -17,8 +17,35 @@ public class ZoneWindow extends JFrame {
 
         addWindowListener(l);
 
-        ImageIcon img = new ImageIcon("tips.gif");
-        JButton bouton = new JButton("Fin de tour", img);
+        //ImageIcon img = new ImageIcon("tips.gif");
+        JButton bouton = new JButton("Fin de tour");
+        bouton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                ile.finDeTour();
+                System.out.println("ok");
+                JPanel panneau = new JPanel() {
+                    @Override
+                    public void paintComponent(Graphics g) {
+                        super.paintComponent(g);
+                        g.setColor(Color.BLACK);
+                        for (int x=0; x<ile.taille(); x++){
+                            for (int y=0; y< ile.taille(); y++) {
+                                if (ile.grille()[x][y].etat() == Etat.Normale)g.setColor(Color.ORANGE.brighter());
+                                else if (ile.grille()[x][y].etat() == Etat.Inondee)g.setColor(Color.GRAY);
+                                g.fillRect(105+x*100, 105+y*100, 90, 90);
+                                g.setColor(Color.BLACK);
+                            }
+                        }
+
+                    }
+                };
+                panneau.add(bouton);
+                setContentPane(panneau);
+                setVisible(true);
+            }
+        });
 
         JPanel panneau = new JPanel() {
             @Override
