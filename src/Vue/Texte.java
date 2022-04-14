9bb2df44fd2dfc4ae7433085198b27389314f18b
Bruclor <102045677+Bruclor@users.noreply.github.com>
2022-04-14 21:16:32 +0200

    Update Texte.java

--- src/Vue/Texte.java
+++ src/Vue/Texte.java
@@ -15,4 +15,11 @@ public class Texte extends JLabel {
         this.setPreferredSize(dimension);
     }
 
+    public Texte(String text, Color color, Dimension dimension, int taille){
+        this.setText(text);
+        this.setForeground(color);
+        this.setPreferredSize(dimension);
+        this.setFont(new Font("Serif", Font.CENTER_BASELINE, taille));
+    }
+
 }
