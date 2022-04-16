5e1b112f3052eea5cb3d8aa536d656dfe321afc5
Bruclor <102045677+Bruclor@users.noreply.github.com>
2022-04-16 19:45:43 +0200

    Update Coord.java

--- src/Modeles/Coord.java
+++ src/Modeles/Coord.java
@@ -1,25 +1,32 @@
 package Modeles;
 
 import java.util.Random;
+import java.util.Vector;
 
-/**
- * Coordonnée dans un plan
- **/
+/* =============================================
+ * =                                           =
+ * =              CLASSE COORD                 =
+ * =                                           =
+ * =============================================
+ */
 public class Coord {
 
-    /*******************/
-    /**   Attributs   **/
-    /*******************/
+    /*
+      ===========================================
+      =                ATTRIBUTS                =
+      ===========================================
+     */
 
-    private int x;
-    private int y;
+    private int x;           //X
+    private int y;           //Y
 
-    /*******************/
-    /** Constructeur  **/
-    /*******************/
+    /*
+      ===========================================
+      =              CONSTRUCTEUR               =
+      ===========================================
+     */
 
-    /**
-     * Constructeur
+    /** -- Constructeur
      *
      * @param x Coordonnée abscisse
      * @param y Coordonnée ordonnée
@@ -29,8 +36,7 @@ public class Coord {
         this.y = y;
     }
 
-    /**
-     * Constructeur aléatoire
+    /** -- Constructeur aléatoire
      *
      * @param sup Coordonnée max pour x et y
      **/
@@ -40,28 +46,73 @@ public class Coord {
         this.y = random.nextInt(sup);
     }
 
-    /*******************/
-    /**    Getter     **/
-    /*******************/
+    /*
+      ===========================================
+      =                GETTER                   =
+      ===========================================
+     */
 
-    /**
-     * Getter x
+    /** -- Coordonnée x
      *
-     * @return Coordonnée x
+     * @return coord x
      **/
     public int x(){return this.x;}
 
-    /**
-     * Getter x
+    /** -- Coordonnée y
      *
-     * @return Coordonnée x
+     * @return coord y
      **/
     public int y(){return this.y;}
 
-    public void set_x(int x){this.x = x;}
-    public void set_y(int y){this.y = y;}
-
+    /** -- Indique si une coordonnée est égale a une autre
+     *
+     * @param other
+     *
+     * @return booleen vrai si this et other sont egaux
+     */
     public boolean equals(Coord other){
         return (this.x()==other.x() && this.y()==other.y());
     }
+
+    /** -- Retourne le vecteur des coordonnées adjacentes
+     *
+     * @param inf la borne inférieur (incluse)
+     * @param sup la borne supérieur (non incluse)
+     *
+     * @return vecteur des zones autour de la zone
+     **/
+    public Vector<Coord> adjacents(int inf, int sup) {
+        Vector<Coord> adj = new Vector<Coord>();
+        if (this.x() < sup - 1) {
+            adj.add(new Coord(this.x() + 1, this.y()));
+        }
+        if (this.x() > inf) {
+            adj.add(new Coord(this.x() - 1, this.y()));
+        }
+        if (this.y() < sup - 1) {
+            adj.add(new Coord(this.x(), this.y() + 1));
+        }
+        if (this.y() > inf) {
+            adj.add(new Coord(this.x(), this.y() - 1));
+        }
+        return adj;
+    }
+
+    /*
+      ===========================================
+      =                SETTER                   =
+      ===========================================
+     */
+
+    /** -- Change la coordonnée x
+     *
+     * @param x nouvelle coord
+     */
+    public void set_x(int x){this.x = x;}
+
+    /** -- Change la coordonnée y
+     *
+     * @param y nouvelle coord
+     */
+    public void set_y(int y){this.y = y;}
 }
