eadcd89a795ef519604f00aff82647bbb2525359
Bruclor <102045677+Bruclor@users.noreply.github.com>
2022-04-16 19:54:30 +0200

    Update Panneau.java

--- src/Vue/Panneau.java
+++ src/Vue/Panneau.java
@@ -3,16 +3,39 @@ package Vue;
 import javax.swing.*;
 import java.awt.*;
 
+
+/* =============================================
+ * =                                           =
+ * =             CLASS PANNEAU                 =
+ * =                                           =
+ * =============================================
+ */
 public class Panneau extends JPanel {
 
-    private String nom;
+    /*
+      ===========================================
+      =              CONSTRUCTEURS              =
+      ===========================================
+     */
 
-    public Panneau(String nom, Color fontColor){
+    /** -- Construit un panneau avec dimension adaptee
+     *
+     * @param fontColor la couleur de fond
+     */
+    public Panneau(Color fontColor){
         this.setBackground(fontColor);
         this.setLayout(new FlowLayout());
     }
 
-    public void addBouton(Bouton b){
-        this.add(b);
+    /** -- Construit un panneau avec dimension personnalisee
+     *
+     * @param fontColor la couleur de fond
+     * @param dimension dimension du panneau
+     */
+    public Panneau(Color fontColor, Dimension dimension){
+        this.setBackground(fontColor);
+        this.setLayout(new FlowLayout());
+        this.setPreferredSize(dimension);
     }
+
 }
