082106b45c155a8d536fe46b4959101742d280fe
Bruclor <102045677+Bruclor@users.noreply.github.com>
2022-04-16 19:43:20 +0200

    Update Controleur.java

--- src/Controleurs/Controleur.java
+++ src/Controleurs/Controleur.java
@@ -1,5 +1,6 @@
 package Controleurs;
 
+import Modeles.ChangeParametre;
 import Modeles.Coord;
 import Modeles.Ile;
 import Vue.Fenetre;
@@ -10,30 +11,35 @@ import java.awt.event.ActionListener;
 
 public class Controleur implements ActionListener {
 
-    //Controle sur une fenetre et un modele
-    private Fenetre fenetre;
-    private Ile ile;
 
-    //Construction
+    private Fenetre fenetre;                //Une fenetre d'affichage (Vue)
+    private Ile ile;                        //Une ile (Modele)
+
+    /** -- Constructeur : initialise une ile et une fenetre
+     */
     public Controleur(){
-        this.ile = new Ile(6, 1, 1);
+        this.ile = new Ile( 2, 2);
         this.fenetre = new Fenetre();
         this.initActionListener();
     }
 
-    //Boutons fonctionnels
+    /** -- Ajoute un ActionListener aux boutons
+     */
     public void initActionListener(){
         for (int k=0; k<fenetre.boutons().size(); k++){
             fenetre.bouton(k).addActionListener(this);
         }
     }
 
-    //gestion des évènements
+    /** -- Gere les évènements
+     *
+     * @param e un evenement enclenché par l'utilisateur
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source =  e.getSource();
-        if (fenetre.bouton(0).equals(source)) { //Nouvelle partie
-            this.ile = new Ile(6, ile.getNbJoueurs(), ile.getNbArtefacts());  //Modification données (Modele)
+        if (fenetre.bouton(0).equals(source)) {                                  //Nouvelle partie
+            this.ile = new Ile(ile.getNbJoueurs(), ile.getNbArtefacts());   //Modification données (Modele)
             this.ile.setBackground(Color.BLUE.darker().darker());
             this.ile.setPreferredSize(new Dimension(600, 600));
             this.ile.InitJoueurs();
@@ -42,69 +48,88 @@ public class Controleur implements ActionListener {
             this.fenetre.initActions();
             this.fenetre.actualiseInventaire(this.ile);
             this.fenetre.actualise(this.ile);
-        } else if (fenetre.bouton(1) == source) { //Parametres
+        } else if (fenetre.bouton(1) == source) {                                 //Choix des parametres
             this.fenetre.setContentPane(fenetre.Parametres());
-        } else if (fenetre.bouton(2) == source) { //Retour
+        } else if (fenetre.bouton(2) == source) {                                 //Retour au menu
             this.fenetre.setContentPane(fenetre.Menu());
-        } else if (fenetre.bouton(3) == source) { //Nombre de Joueurs
+        } else if (fenetre.bouton(3) == source) {                                 //Nombre de Joueurs
             this.ile.parametrage(ChangeParametre.Joueurs);
             this.fenetre.setContentPane(fenetre.Nombres());
-        } else if (fenetre.bouton(4) == source) { //Nombre d'artefacts
+        } else if (fenetre.bouton(4) == source) {                                 //Nombre d'artefacts
             this.ile.parametrage(ChangeParametre.Artefacts);
             this.fenetre.setContentPane(fenetre.Nombres());
-        } else if (fenetre.bouton(5) == source) { //1
+        } else if (fenetre.bouton(5) == source) {                                 //select 1
             this.ile.operateParam(1);
             this.fenetre.setContentPane(fenetre.Parametres());
-        } else if (fenetre.bouton(6) == source) { //2
+        } else if (fenetre.bouton(6) == source) {                                 //select 2
             this.ile.operateParam(2);
             this.fenetre.setContentPane(fenetre.Parametres());
-        } else if (fenetre.bouton(7) == source) { //3
+        } else if (fenetre.bouton(7) == source) {                                 //select 3
             this.ile.operateParam(3);
             this.fenetre.setContentPane(fenetre.Parametres());
-        } else if (fenetre.bouton(8) == source) { //4
+        } else if (fenetre.bouton(8) == source) {                                 //select 4
             this.ile.operateParam(4);
             this.fenetre.setContentPane(fenetre.Parametres());
-        } else if (fenetre.bouton(9) == source) { //Arreter la partie
+        } else if (fenetre.bouton(9) == source) {                                 //Arreter la partie
             this.fenetre.setContentPane(fenetre.Menu());
-        } else if (fenetre.bouton(10) == source) { //Fin de tour
-            this.ile.finDeTour();
+        } else if (fenetre.bouton(10) == source) {                                //Cherche Clé/Fin de tour
+            this.ile.chercheCle();                      //cherche une clé
+            this.ile.finDeTour();                       //fin du tour
+            this.fenetre.actualiseInventaire(this.ile); //actualisation inventaire
+            if (ile.GameOver()){this.fenetre.GameOver(this.ile.infoGameOver());} //defaite après inondation ?
+            this.fenetre.actualise(this.ile);           //actualisation fenetre
+        } else if (fenetre.bouton(11) == source) {                               //Recuperer artefact
+            this.ile.artefactRecupere();                //recuperation artefact
+            this.fenetre.actualiseInventaire(this.ile); //actualisation inventaire
+            this.fenetre.actualise(this.ile);           //actualisation fenetre
+        } else if (fenetre.bouton(12) == source){                                //Se deplacer vers le haut
+            this.ile.deplace(new Coord(-1, 0));   //deplacement
+            this.fenetre.actualiseInventaire(this.ile); //actualisation inventaire
+            if (ile.IsWin()){this.fenetre.Victory();}   //victoire ?
+            this.fenetre.actualise(this.ile);           //actualisation fenetre
+        } else if (fenetre.bouton(13) == source){                                //Se deplacer vers le bas
+            this.ile.deplace(new Coord(1, 0));     //idem
             this.fenetre.actualiseInventaire(this.ile);
-            if (ile.GameOver()){
-                System.out.println("Game Over");
-                this.fenetre.GameOver();
-            }
+            if (ile.IsWin()){this.fenetre.Victory();}
             this.fenetre.actualise(this.ile);
-        } else if (fenetre.bouton(11) == source) { //Chercher une clé
-            this.ile.chercheCle();
+        } else if (fenetre.bouton(14)==source){                                  //Se deplacer vers la droite
+            this.ile.deplace(new Coord(0, 1));     //idem
             this.fenetre.actualiseInventaire(this.ile);
-            if (ile.GameOver()){
-                System.out.println("Game Over");
-                this.fenetre.GameOver();
-            }
+            if (ile.IsWin()){this.fenetre.Victory();}
             this.fenetre.actualise(this.ile);
-        } else if (fenetre.bouton(12) == source){
-            this.ile.deplace(new Coord(-1, 0));
+        } else if (fenetre.bouton(15)==source){                                  //Se deplacer vers la gauche
+            this.ile.deplace(new Coord(0, -1));    //idem
             this.fenetre.actualiseInventaire(this.ile);
+            if (ile.IsWin()){this.fenetre.Victory();}
             this.fenetre.actualise(this.ile);
-        } else if (fenetre.bouton(13) == source){
-            this.ile.deplace(new Coord(1, 0));
+        } else if (fenetre.bouton(16) == source){                                //Assecher en haut
+            this.ile.asseche(new Coord(-1, 0));
             this.fenetre.actualiseInventaire(this.ile);
             this.fenetre.actualise(this.ile);
-        } else if (fenetre.bouton(14)==source){
-            this.ile.deplace(new Coord(0, 1));
+        } else if (fenetre.bouton(17) == source){                                 //Assecher en bas
+            this.ile.asseche(new Coord(1, 0));
             this.fenetre.actualiseInventaire(this.ile);
             this.fenetre.actualise(this.ile);
-        } else if (fenetre.bouton(15)==source){
-            System.out.println("le bouton gauche est clique");
-            this.ile.deplace(new Coord(0, -1));
+        } else if (fenetre.bouton(18)==source){                                   //Assecher a droite
+            this.ile.asseche(new Coord(0, 1));
+            this.fenetre.actualiseInventaire(this.ile);
+            this.fenetre.actualise(this.ile);
+        } else if (fenetre.bouton(19)==source){                                   //Assecher a gauche
+            this.ile.asseche(new Coord(0, -1));
+            this.fenetre.actualiseInventaire(this.ile);
+            this.fenetre.actualise(this.ile);
+        } else if (fenetre.bouton(20)==source){                                   //Assecher sur place
+            this.ile.asseche(new Coord(0, 0));
             this.fenetre.actualiseInventaire(this.ile);
             this.fenetre.actualise(this.ile);
         }
-        this.fenetre.setVisible(true);
+        this.fenetre.setVisible(true);                                               //Afficher
     }
 
-    //Fonction main
+    /** --FONCTION MAIN() : Lance l'application
+     */
     public static void main(String[] args){
-        Controleur control = new Controleur();   //Controleur
+        Controleur control = new Controleur();
     }
+
 }
