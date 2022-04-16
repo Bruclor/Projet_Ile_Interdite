6ae86d5db15ba9f3bd91d9abfc78169c45b2879e
Bruclor <102045677+Bruclor@users.noreply.github.com>
2022-04-16 19:50:06 +0200

    Update Bouton.java

--- src/Vue/Bouton.java
+++ src/Vue/Bouton.java
@@ -3,19 +3,42 @@ package Vue;
 import javax.swing.*;
 import java.awt.*;
 
+/* =============================================
+ * =                                           =
+ * =              CLASS BOUTON                 =
+ * =                                           =
+ * =============================================
+ */
 public class Bouton extends JButton {
 
-    public Bouton(String nom, Color fontColor){
-        this.setText(nom);
-        this.setBackground(fontColor);
-    }
+    /*
+      ===========================================
+      =             CONSTRUCTEURS               =
+      ===========================================
+     */
 
-    public Bouton(String nom, Color fontColor, Dimension dimension){
-        this.setText(nom);
+    /** -- Construit un bouton avec du texte
+     * @param texte texte sur le bouton
+     * @param fontColor coulueur de fond du bouton
+     * @param dimension dimension du bouton
+     *
+     **/
+    public Bouton(String texte, Color fontColor, Dimension dimension){
+        this.setText(texte);
         this.setBackground(fontColor);
         this.setPreferredSize(dimension);
     }
 
-    public void setColor(Color color){this.setBackground(color);}
+    /** -- Construit un bouton avec une image
+     * @param icon image sur le bouton
+     * @param fontColor coulueur de fond du bouton
+     * @param dimension dimension du bouton
+     *
+     **/
+    public Bouton(ImageIcon icon, Color fontColor, Dimension dimension){
+        this.setIcon(icon);
+        this.setBackground(fontColor);
+        this.setPreferredSize(dimension);
+    }
 
 }
