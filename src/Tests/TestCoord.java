a5b076a83efe5e9ced22780481b4dbd4991b16e0
alexo <alexou.loret@gmail.com>
2022-04-17 17:27:05 +0200

    test coord et zone

--- src/Tests/TestCoord.java
+++ src/Tests/TestCoord.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import Modeles.Coord;
 
+import java.util.Vector;
+
 public class TestCoord {
     @Test
     public void testConstCoord(){
@@ -29,8 +31,27 @@ public class TestCoord {
     public void testEqual(){
         int x = 2;
         int y = 3;
-        Coord c = new Coord(x,y);
-        Coord d = new Coord(x,y);
-        assertTrue(c.equals(d));
+        Coord c1 = new Coord(x,y);
+        Coord c2 = new Coord(x,y);
+        assertTrue(c1.equals(c2));
+    }
+
+    @Test
+    public void testAdjacent(){
+        Coord c1 = new Coord(0,0);
+        Vector<Coord> adj1 = c1.adjacents(0,6);
+        Coord c2 = new Coord(1,0);
+        Coord c3 = new Coord(0,1);
+        assertTrue(adj1.get(0).equals(c2));
+        assertTrue(adj1.get(1).equals(c3));
+
+        Coord c4 = new Coord(1,1);
+        Vector<Coord> adj2 = c4.adjacents(0,6);
+        Coord c5 = new Coord(2,1);
+        Coord c6 = new Coord(1,2);
+        assertTrue(adj2.get(0).equals(c5));
+        assertTrue(adj2.get(1).equals(c3));
+        assertTrue(adj2.get(2).equals(c6));
+        assertTrue(adj2.get(3).equals(c2));
     }
 }
--- src/Tests/TestIle.java
+++ src/Tests/TestIle.java
@@ -74,4 +74,57 @@ public class TestIle {
         assertFalse(i.getArtefactsRecuperes().get(Artefact.Eau));
         assertFalse(i.getArtefactsRecuperes().get(Artefact.Terre));
     }
+
+    @Test
+    public void testDeplace() {
+        int nbJ = 2;
+        int nbA = 4;
+        Ile i = new Ile(nbJ, nbA);
+        i.InitJoueurs();
+        i.InitArtefacts();
+        int x = 1;
+        int y = 1;
+        Coord c =new Coord(x,y);
+        i.getJoueur(0).deplace(c);
+        assertEquals(i.getJoueur(0).x(),x);
+        assertEquals(i.getJoueur(0).y(),y);
+        Coord v1 = new Coord(-1,0);
+        Coord v2 = new Coord(-10,-10);
+        Coord v3 = new Coord(1,0);
+        i.deplace(v1);
+        assertEquals(i.getJoueur(0).x(),x);
+        assertEquals(i.getJoueur(0).y(),y);
+        i.deplace(v2);
+        assertEquals(i.getJoueur(0).x(),x);
+        assertEquals(i.getJoueur(0).y(),y);
+        i.deplace(v3);
+        assertEquals(i.getJoueur(0).x(),x+1);
+        assertEquals(i.getJoueur(0).y(),y);
+        assertEquals(i.getJoueur(0).getNbActions(),2);
+    }
+
+    @Test
+    public void testAsseche(){
+        int nbJ = 2;
+        int nbA = 4;
+        Ile i = new Ile(nbJ, nbA);
+        i.InitJoueurs();
+        i.InitArtefacts();
+        int x = 1;
+        int y = 1;
+        Coord c =new Coord(x,y);
+        i.getJoueur(0).deplace(c);
+        i.zone(2,1).inonde();
+        Coord v1 = new Coord(-1,0);
+        Coord v2 = new Coord(0,1);
+        Coord v3 = new Coord(1,0);
+        i.asseche(v1);
+        assertEquals(i.zone(0,1).etat(),Etat.Submergee);
+        i.asseche(v2);
+        assertEquals(i.zone(1,2).etat(),Etat.Normale);
+        i.asseche(v3);
+        assertEquals(i.zone(2,1).etat(),Etat.Normale);
+        assertEquals(i.getJoueur(0).getNbActions(),2);
+    }
 }
+
