3fa458d33c8f5e6e2b411a67af03cb3a7101802b
Bruclor <102045677+Bruclor@users.noreply.github.com>
2022-04-23 23:49:23 +0200

    Create Objet.java

---
+++ src/Modeles/Objet.java
@@ -0,0 +1,10 @@
+package Modeles;
+
+/* =============================================
+ * =                                           =
+ * =             ENUM OBJET                    =
+ * =                                           =
+ * =============================================
+ */
+public enum Objet {CleFeu, CleEau, CleAir, CleTerre, SacDeSable, Helicoptere, MonteeDesEaux, Rien}
+
