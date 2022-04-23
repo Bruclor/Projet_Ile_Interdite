14146d5415a62c3f9852ec3cca5e6e87b562f49a
Bruclor <102045677+Bruclor@users.noreply.github.com>
2022-04-23 23:46:38 +0200

    Delete ChangeParametre.java

--- src/Modeles/ChangeParametre.java
+++
@@ -1,9 +0,0 @@
-package Modeles;
-
-/* =============================================
- * =                                           =
- * =          ENUM CHANGEPARAMETRE             =
- * =                                           =
- * =============================================
- */
-public enum ChangeParametre {Joueurs, Artefacts};
